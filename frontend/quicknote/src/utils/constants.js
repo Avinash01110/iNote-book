016c154c35fdff13fcde49148c8a353b2f8bc9b0
Avinash01110 <gavinash321098@gmail.com>
2024-10-08 19:16:39 +0530

    v 1.7

--- frontend/quicknote/src/utils/constants.js
+++ frontend/quicknote/src/utils/constants.js
@@ -1,2 +1,3 @@
 // export const BASE_URL = "http://localhost:8000/api"
-export const BASE_URL = `${import.meta.env.VITE_GEMINI_BASE_URL}`
+// export const BASE_URL = `${import.meta.env.VITE_GEMINI_BASE_URL}`
+export const BASE_URL = "https://i-notebook-back.vercel.app/api"
